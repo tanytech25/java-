5fdbc8413ceb33e87a55f30c10a7e9191f829dc0
Tanya agrawal <tanya.agrawal_cs24@gla.ac.in>
2025-08-07 21:08:03 +0530

    Update and rename github.java.txt to github.java

---
+++ github.java
@@ -0,0 +1,13 @@
+public class Pattern0 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int star =5;
+        int i = 1;
+        while (i<=star) {
+        	System.out.print("*");
+        	i++;
+        }
+	}
+
+}
